594aad1403625fca5f89a57919344ad5d63977de
changil kim <kci0127@naver.com>
2022-03-01 16:20:26 +0900

    third commit
    날씨 API를 이용해 현재 지역의 날씨와 주간날씨 표시 기능 추가, 날씨 설명추가, 아이콘 추가, 디자인 일부 수정

--- App.js
+++ App.js
@@ -1,14 +1,32 @@
 import * as Location from "expo-location";
 import { useEffect, useState } from "react";
-import { View, Dimensions, StyleSheet, Text, ScrollView } from "react-native";
+import {
+  View,
+  Dimensions,
+  StyleSheet,
+  Text,
+  ScrollView,
+  ActivityIndicator,
+} from "react-native";
+import { Fontisto } from "@expo/vector-icons";
 
 const { width: SCREEN_WIDTH } = Dimensions.get("window");
+const API_KEY = "4441921070b8b220c64b1537072ff106";
+const icons = {
+  Clouds: "cloudy",
+  Clear: "day-sunny",
+  Atmosphere: "cloudy-gusts",
+  Snow: "snow",
+  Rain: "rains",
+  Drizzle: "rain",
+  Thunderstorm: "lightning",
+};
 
 export default function App() {
   const [city, setCity] = useState("Loading...");
-  const [location, setLocation] = useState();
+  const [days, setDays] = useState([]);
   const [ok, setOk] = useState(true);
-  const ask = async () => {
+  const getWeather = async () => {
     const { granted } = await Location.requestForegroundPermissionsAsync();
     if (!granted) {
       setOk(false);
@@ -18,13 +36,19 @@ export default function App() {
     } = await Location.getCurrentPositionAsync({ accuracy: 5 });
     const location = await Location.reverseGeocodeAsync(
       { latitude, longitude },
-      { useGooglemap: false }
+      { useGoogleMap: false }
     );
     setCity(location[0].city);
+
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alert&appid=${API_KEY}&units=metric`
+    );
+    const json = await response.json();
+    setDays(json.daily);
   };
 
   useEffect(() => {
-    ask();
+    getWeather();
   }, []);
   return (
     <View style={styles.container}>
@@ -37,36 +61,66 @@ export default function App() {
         showsHorizontalScrollIndicator={false}
         contentContainerStyle={styles.weather}
       >
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
+        {days.length === 0 ? (
+          <View style={{ ...styles.day, alignItems: "center" }}>
+            <ActivityIndicator
+              color="white"
+              size="large"
+              style={{ marginTop: 10 }}
+            />
+          </View>
+        ) : (
+          days.map((day, index) => (
+            <View key={index} style={styles.day}>
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignItems: "flex-end",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Text style={styles.temp}>
+                  {parseFloat(day.temp.day).toFixed(1)}
+                </Text>
+                <Text style={styles.celcious}>℃</Text>
+                <Fontisto
+                  name={icons[day.weather[0].main]}
+                  size={70}
+                  color="black"
+                />
+              </View>
+              <Text style={styles.description}>{day.weather[0].main}</Text>
+              <Text style={styles.tinyText}>{day.weather[0].description}</Text>
+            </View>
+          ))
+        )}
       </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1.2, backgroundColor: "tomato" },
+  container: { flex: 1.2, backgroundColor: "#FBBD99" },
   city: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
   },
-  cityName: { fontSize: 68, fontWeight: "500" },
+  cityName: {
+    fontSize: 68,
+    fontWeight: "500",
+  },
   weather: {},
   day: {
     width: SCREEN_WIDTH,
     alignItems: "center",
   },
-  temp: { fontSize: 158, marginTop: 50, fontWeight: "600" },
-  description: { fontSize: 60, marginTop: -30 },
+  temp: { fontSize: 100, marginTop: 50, fontWeight: "600" },
+  description: { fontSize: 40, marginTop: 10 },
+  tinyText: {
+    fontSize: 20,
+  },
+  celcious: {
+    fontSize: 30,
+  },
 });
